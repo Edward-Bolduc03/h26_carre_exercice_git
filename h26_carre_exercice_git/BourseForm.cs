18f8d2a5e36a473addd9d9446d6712a133de7f2d
Ian McDiarmid <darth_ian@gmail.com>
2026-01-26 16:48:54 -0500

    Add fields for stock value and bank amount
    Ajout de variables concernant la bourse

--- h26_carre_exercice_git/BourseForm.cs
+++ h26_carre_exercice_git/BourseForm.cs
@@ -2,6 +2,10 @@ namespace h26_carre_exercice_git
 {
     public partial class BourseForm : Form
     {
+        decimal valeurAction;
+        int nombreActions;
+        decimal montantEnBanque;
+
         public BourseForm()
         {
             InitializeComponent();
